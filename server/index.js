8c1a86db09d2bdf0a18a68ffa3cf8d852eb999de
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-08-31 17:27:11 -0400

    edit 05

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 
 // app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'));
-});
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../index.html'));
+// });
 
-app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/style.css'))
-  });
+// app.get('/css', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/style.css'))
+//   });
   
 
 
