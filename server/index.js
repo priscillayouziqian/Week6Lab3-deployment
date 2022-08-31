fe2d2de0113c81a4092a12ed7ce93cf736de5f2e
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-08-31 17:19:55 -0400

    edit 03

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/style.css'))
+    res.sendFile(path.join(__dirname, '../public/style.css'))
   });
   
 
