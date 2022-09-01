65fd1f7e1045ae49a601d5f69841165d3332bbb7
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-09-01 14:23:26 -0400

    attempt 1

--- server/index.js
+++ server/index.js
@@ -3,7 +3,26 @@ const path = require('path');
 
 const app = express();
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '50b4a9d0111a43d5ab0d61aa2e90ece7',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use('/', express.static(path.join(__dirname, '../client')));
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  }
 
 // app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
 
